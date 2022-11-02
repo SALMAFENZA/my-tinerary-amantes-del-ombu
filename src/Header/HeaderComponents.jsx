6030368e3c53f0c759b70e3388f7812329fe7f02
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-02 14:15:57 -0300

    start of sprint1

--- src/Header/HeaderComponents.jsx
+++ src/Header/HeaderComponents.jsx
@@ -1,1 +1,19 @@
+import React from 'react'
 
+export default function HeaderComponents() {
+    return (
+        <header>
+            <ul>
+                <li>
+                    <a href="-">Home</a>
+                </li>
+                <li>
+                    <a href="-">Home</a>
+                </li>
+            </ul>
+            <div>
+                
+            </div>
+        </header>
+    )
+}
