fe03661e61d0efed1c4be32ba1a41a33df3d51e9
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-02 17:58:38 -0300

    creation of data

--- src/Componentes/Header/HeaderComponents.jsx
+++ src/Componentes/Header/HeaderComponents.jsx
@@ -8,7 +8,7 @@ export default function HeaderComponents() {
                     <a href="-">Home</a>
                 </li>
                 <li>
-                    <a href="-">Home</a>
+                    <a href="-">Hotel</a>
                 </li>
             </ul>
             <div>
---
+++ src/Componentes/PagePosition/PagePosition.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PagePosition() {
+    return (
+        <div>
+            PagePosition
+        </div>
+    )
+}
