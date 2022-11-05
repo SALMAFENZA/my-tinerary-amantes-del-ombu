9aa9fe06b07ab1a78333da743c40757513dc6037
salmafenza <salmafenxza@gmail.com>
2022-11-05 18:08:09 -0300

    MT-F2-06 404 PAGE NOT Found w layout and boton to main page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import React from 'react';
-import {Header} from './componentes/Header';
-import imagen from '../src/logo.jpg';
-import Slider from './componentes/Slider/Slider';
-import EventsCarousel from './data/dataCities.js';
-import {Footer} from './componentes/Footer';
-import {FooterToTop} from './componentes/Arrow';
+// import {Header} from './componentes/Header';
+// import imagen from '../src/logo.jpg';
+// import Slider from './componentes/Slider/Slider';
+// import EventsCarousel from './data/dataCities.js';
+// import {Footer} from './componentes/Footer';
+// import {FooterToTop} from './componentes/Arrow';
+import Error404 from "./componentes/Error404";
 
 function App() {
 
   return (
-       <>
-    <EventsCarousel/>
-    {<Footer/>}
-    {<FooterToTop/>}
-    </>
+   <>
+    {<Error404/>}
+   </>
+    // {<EventsCarousel/>}
+    // {<Footer/>}
+    // {<FooterToTop/>}
   );
 }
 
