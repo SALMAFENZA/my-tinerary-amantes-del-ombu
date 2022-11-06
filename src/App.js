6e48206b9b9ebf0f9ca0649a095dd2fe33fe42c2
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-06 02:03:37 -0300

    add mt1-f1-01

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import  SignUp  from "./Componentes/SignUp";
 export default function App() {
   return (
     <>
-        <header/>
+        
         {<SignUp/>}
+
     </>
   );
 }
--- src/Componentes/CityCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CityCard() {
-    return (
-        <div className='cont-card'>
-            
-        </div>
-    )
-}
